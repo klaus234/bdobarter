e697364e65b88cb451e9a2b2cf8d23df3e678c10
klaus234 <klausmaster@gmail.com>
2022-04-30 02:00:18 -0300

    nivel agregado

---
+++ puntos_menu.js
@@ -0,0 +1,58 @@
+const menu_puntos = document.getElementById("contenedor-puntos");
+
+const data_puntos = 
+
+{
+   4 : ["ILIYA", "AJIR", "PUJARA", "NARVO", "BAREMI", "ORFFS", "OBEN", "PADIX"],
+   3 : ["ALNAHA", "TULU", "LISZ", "DATON", "TIGRIS", "ALMAI", "SHASHA", "ROSEVAN", "ORISHA"]
+}
+function nuevoNivel(num)
+{
+    const li = document.createElement("li");
+    li.innerText = "Nivel " + num;
+    li.menulvl = num;
+
+    li.onclick = menu_cargar_puntos;
+    return li;
+}
+function nuevoPunto(nombre)
+{
+    const li = document.createElement("li");
+    li.innerText = nombre;
+    li.className = "puntomenu"
+    li.onclick = function()
+    {
+        const nodosm_area = document.getElementById("nodosm");
+        nodosm_area.value += this.innerText + "\n";
+    }
+    return li;
+}
+function menu_cargar_puntos()
+{
+    menu_puntos.innerHTML = "";
+    const atras = document.createElement("li");
+    atras.innerText = "<< Atras";
+    atras.style = "color: red;";
+    atras.className = "puntomenu";
+    atras.onclick = function()
+    {
+        mostrarNiveles();
+    }
+    menu_puntos.appendChild(atras);
+
+    data_puntos[this.menulvl].map((elemento) =>
+    {
+        menu_puntos.appendChild(nuevoPunto(elemento));
+    });
+
+    
+}
+function mostrarNiveles()
+{    
+    menu_puntos.innerHTML = "";
+    menu_puntos.appendChild(nuevoNivel(1));
+    menu_puntos.appendChild(nuevoNivel(2));
+    menu_puntos.appendChild(nuevoNivel(3));
+    menu_puntos.appendChild(nuevoNivel(4));
+}
+mostrarNiveles();
